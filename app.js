35bb16156680f8cc90da521a55bfddbcdd9f2d3c
sindhu11042000 <sindhu.akula@slu.edu>
2025-04-22 23:24:10 +0000

    commit

--- app.js
+++ app.js
@@ -22,3 +22,9 @@ app.post('/products', api.createProduct)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
+// app.js
+
+// ...
+
+app.get('/orders', api.listOrders)
+app.get('/orders/', api.createOrder)
